9ffd7eaf9c12cf61a758c5649b11be1afc66db85
Maksym <stud_pm22_11@zfkkt.org.ua>
2026-04-02 21:13:46 +0200

    Sorted pages

--- src/app/pages/account/profile/profile.component.ts
+++ src/app/pages/account/profile/profile.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [RouterLink, RouterOutlet, CommonModule],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
